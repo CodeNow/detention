fcfc6ec9431ed6a464de937a5b6eb20cbc1eaedd
Myztiq <kahn@runnable.com>
2015-09-02 15:52:19 -0700

    Fixed display so we can change the tense for buildFailed.

--- app.js
+++ app.js
@@ -44,6 +44,11 @@ app.route('/*').get(function (req, res, next) {
       }
       options.ports = value;
     }
+    if (options.status === 'buildFailed') {
+      options.status = 'has failed it\'s build';
+    } else {
+      options.status = 'is ' + options.status;
+    }
     res.render('pages/' + page, options);
   } else {
     res.render('pages/invalid', options);
