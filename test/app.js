af96a8379d1dfe98ccab5f226a73857c98b6cfa5
Casey Flynn <cflynn.us@gmail.com>
2015-11-26 12:51:24 +0800

    Seed test files and package script

---
+++ test/app.js
@@ -0,0 +1,16 @@
+/**
+ * @module test/app
+ */
+'use strict';
+
+var Lab = require('lab');
+var Code = require('code');
+
+var lab = exports.lab = Lab.script();
+
+var describe = lab.describe;
+var expect = Code.expect;
+var it = lab.test;
+
+describe('app.js', function () {
+});
